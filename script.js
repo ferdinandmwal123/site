367ffb8c82dd95998db21670e9cce4a2eedd3589
ferdinandmwal123 <ferdinandmwal@gmail.com>
2025-03-06 20:50:59 -0500

    added footer and side image

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 (function () {
     'use strict';
+
+    /******************************************************************************
+    Copyright (c) Microsoft Corporation.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+    ***************************************************************************** */
+
     var __assign = function() {
         __assign = Object.assign || function __assign(t) {
             for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -182,13 +198,11 @@
     }
 
     document.addEventListener("DOMContentLoaded", function () {
-        var button = document.getElementById("releaseBalloons");
+      
+        var button = document.getElementById("releastBalloonsButton");
         button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
-            delay(1000); //delay of doom
             balloons();
         });
     });
 
 })();
-
-
